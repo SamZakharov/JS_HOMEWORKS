d8bb5003583a859c0e58d1c6543b3e88d087eaa8
SamZakharov <semen.zaharovv@gmail.com>
2023-12-15 18:29:25 +0200

    HW 2 comleted.
    Added string type check.

--- src/js/script.js
+++ src/js/script.js
@@ -18,6 +18,7 @@ alert('Welcome, ' + userFirstName + ' on our website!');
 const stringPlusNumber = '10' + 10;
 //If we add a string to a number, then we get the string
 // In this case we will get 1010, and not 20, as in the mathematical calculation, because the first "10" is a string. This opportunity is called concatenation.//
+console.log(typeof (stringPlusNumber));
 console.log(stringPlusNumber);
 
 
