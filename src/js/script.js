7f5ef1029efbc6c2239e6f50a1db705c89d19d5a
SamZakharov <semen.zaharovv@gmail.com>
2023-12-15 18:21:58 +0200

    HW 3 comleted.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const firstNumber = +prompt('Put your first number: ');
+const secondNumber = +prompt('Put your second number: ');
+const sumPlus = firstNumber + secondNumber;
+const sumMinus = firstNumber - secondNumber;
+const sumMult = firstNumber * secondNumber;
+const sumDev = firstNumber / secondNumber;
+
+console.log('The result of your operation is: ' + firstNumber + ' + ' + secondNumber + ' = ' + sumPlus);
+console.log('The result of your operation is: ' + firstNumber + ' - ' + secondNumber + ' = ' + sumMinus);
+console.log('The result of your operation is: ' + firstNumber + ' * ' + secondNumber + ' = ' + sumMult);
+console.log('The result of your operation is: ' + firstNumber + ' / ' + secondNumber + ' = ' + sumDev);
+
+
