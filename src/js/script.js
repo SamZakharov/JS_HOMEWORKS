c74f588c4ebf8396341894d0df093f4c97a6b9e7
SamZakharov <semen.zaharovv@gmail.com>
2023-12-15 17:34:06 +0200

    HW 2 comleted Added comments in task 3, as an explanation why it works this way.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+//Task #1//
+const name = 'John';
+const admin = name;
+
+
+alert(admin);
+
+//Task #2//
+const planet = 'Earth';
+const userFirstName = prompt('Put your first name:');
+
+console.log(planet);
+alert('Welcome, ' + userFirstName + ' on our website!');
+
+//Task #3//
+const stringPlusNumber = '10' + 10;
+//If we add a string to a number, then we get the string
+// In this case we will get 1010, and not 20, as in the mathematical calculation, because the first "10" is a string. This opportunity is called concatenation.//
+console.log(stringPlusNumber);
+
+
