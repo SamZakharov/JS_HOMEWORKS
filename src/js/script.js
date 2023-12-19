221f109c243498cbb3227b9feb06c0c95b13cf1b
SamZakharov <semen.zaharovv@gmail.com>
2023-12-19 17:47:37 +0200

    HW 4 comleted.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// //task 1//
+const firstTarget = prompt ('Please put here the number or word: ');
+const secondTarget = prompt ('Please put here the number or word: ');
+const thirdTarget = prompt ('Please put here the number or word: ');
+const sum = firstTarget + secondTarget + thirdTarget;
+
+
+alert(`${firstTarget} + ${secondTarget} + ${thirdTarget} = ${sum}`);
+
+
+//task 2//
+
+const number = 12345;
+
+console.log(number.toString().replaceAll('', ' '));
