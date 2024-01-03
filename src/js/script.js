03525376a77c3dc09c13e077ce3b487e643b96d0
SamZakharov <semen.zaharovv@gmail.com>
2024-01-03 11:03:19 +0200

    Finished hw-15

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const firstNumber = Number(prompt('Enter first number'));
+const secondNumber = Number(prompt('Enter second number'));
+const thirdNumber = Number(prompt('Enter third number'));
+const average = ((firstNumber + secondNumber + thirdNumber) / 3).toFixed(1);
+
+alert('The arithmetic mean of your numbers is ' + average);
