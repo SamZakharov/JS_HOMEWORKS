a5d743b1cb789c868336ac4f8df0dbf1de42155b
SamZakharov <semen.zaharovv@gmail.com>
2024-01-03 09:04:10 +0200

    Finished hw-12

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const firstNumber = +prompt('First number: ');
+const secondNumber = +prompt('Second number: ');
+const sumPlus = firstNumber + secondNumber;
+const sumMinus = firstNumber - secondNumber;
+const sumMult = firstNumber * secondNumber;
+const sumDev = firstNumber / secondNumber;
+
+alert(firstNumber + ' + ' + secondNumber + ' = ' + sumPlus);
+alert(firstNumber + ' - ' + secondNumber + ' = ' + sumMinus);
+alert(firstNumber + ' * ' + secondNumber + ' = ' + sumMult);
+alert(firstNumber + ' / ' + secondNumber + ' = ' + sumDev);
