fcc1934793da0a50ee95ccff591fb9854769b7e8
SamZakharov <semen.zaharovv@gmail.com>
2024-01-03 10:48:12 +0200

    Finished hw-14

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const askHours = +prompt('Please indicate the number of hours:');
+const secPerHour = askHours * 3600;
+
+alert(`There are ${secPerHour} seconds in ${askHours} hours`);
