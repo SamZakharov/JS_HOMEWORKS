423744786c5948395961d631e2dd4e06dc434d24
SamZakharov <semen.zaharovv@gmail.com>
2024-01-09 18:54:16 +0200

    HW 21 is finished.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,90 @@
 'use strict';
+
+// // 1.
+// for (let i = 10; i <= 20; i++) {
+//     console.log(i);
+// }
+//
+//
+// // 2.
+// for (let i = 10; i <= 20; i++) {
+//     console.log(`Квадрат ${i} = ${i * i}`);
+// }
+//
+// // 3.
+//
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`7 * ${i} = ${7 * i}`)
+// }
+//
+// //4.
+//
+// let sum = 0;
+// for (let i = 0; i <= 15; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum);
+//
+// //5.
+// let sum = 0;
+//
+// for (let i = 15; i <= 35; i++){
+//     console.log(sum += i);
+// }
+//
+//
+// //6.
+// let avarage = 0;
+// let sum = 0;
+// let count = 0;
+//
+// for (let i = 1; i <= 500; i++) {
+//      sum += i;
+//      count++
+// }
+//
+// avarage = sum / count;
+// console.log(avarage);
+//
+// // 7.
+//
+// let sum = 0;
+//
+// for (let i = 30; i <= 80; i++) {
+//     if (i % 2 === 0){
+//         sum += i
+//     }
+// }
+// console.log(sum);
+//
+// // 8.
+//
+// for (let i = 100; i <= 200; i++) {
+//     if (i % 3 === 0){
+//         console.log(i);
+//     }
+// }
+
+// //9. 10. 11.
+//
+// let number = +prompt('enter the number');
+// let sum = 0;
+// let count = 0;
+// for (let i = 0; i <= number; i++) {
+//     if (number % i === 0 && i % 2 === 0) {
+//         sum += i;
+//         count++
+//         console.log(i);
+//
+//     }
+// }
+// console.log(sum);
+
+// //12.
+//
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i * j}`);
+//     }
+// }
+
