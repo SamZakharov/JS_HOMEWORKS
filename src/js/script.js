10b77a6ffa9649242ad188fd92c4eed47e190668
SamZakharov <semen.zaharovv@gmail.com>
2024-01-10 13:59:00 +0200

    HW 25 is finished.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+
+let sumOfOddNumbers = 0;
+outerLoop:
+
+    for (let number = 1; number <= 20; number++) {
+
+        if (number % 2 !== 0) {
+            sumOfOddNumbers += number;
+
+        } else {
+            break outerLoop;
+        }
+    }
+
+console.log('Sum of odd numbers from 1 to 20:' + sumOfOddNumbers);
