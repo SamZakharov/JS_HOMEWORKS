4068d131c47da89a0a3f055ca61eb35381031fbb
SamZakharov <semen.zaharovv@gmail.com>
2024-01-10 13:08:09 +0200

    HW 24 is finished.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let num = +prompt("enter the number");
+let arr = [];
+
+if (num > 1 || !isNaN(num)) {
+    for (let i = 2; i <= num; i++) {
+        if (num % i === 0) {
+            arr.push(i);
+        }
+    }
+    console.log('minimal divider is:', Math.min(...arr) + '.');
+} else {
+    console.log(NaN);
+}
+console.log('all dividers: ', arr);
