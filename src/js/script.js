e764e1bae7cf03f2d025e5f9c97bb1f43ed78e37
SamZakharov <semen.zaharovv@gmail.com>
2024-01-10 13:06:00 +0200

    HW 23 is finished.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+
+let sum = 0;
+let sumOfPow = 0;
+const arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++){
+    sum +=arr[i];
+    sumOfPow += Math.pow(arr[i], 2);
+}
+
+console.log(sum);
+console.log(sumOfPow);
+
+
+
