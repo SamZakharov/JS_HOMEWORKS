55916aa7a1a082889b7054452aa2a305268e5bd0
SamZakharov <semen.zaharovv@gmail.com>
2024-01-12 18:14:50 +0200

    HW 27 is finished.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const number = +prompt('Enter the number:', '5');
+const degree = +prompt('Enter the number of the degree:');
+
+function exponentiation(number, degree = 1) {
+    console.log(degree);
+    if (isNaN(number) || isNaN(degree)) return alert('You are entering an incorrect value');
+    else return Math.pow(number, degree);
+
+}
+
+let result = exponentiation(number, degree);
+
+alert(result);
