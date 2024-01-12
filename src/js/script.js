1f227fb11ed32424eb1debc91d7f99d9702cf87a
SamZakharov <semen.zaharovv@gmail.com>
2024-01-12 17:15:59 +0200

    HW 26 finished.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const lenght = +prompt('Enter the number of symbols', "10");
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const charactersLength = characters.length;
+function generateKey(length, characters) {
+    let key = '';
+    for (let i = 0; i < length; i++) {
+       const index = Math.floor(Math.random() * charactersLength);
+        key += characters[index];
+    }
+    return key;
+
+}
+
+const randomChar = generateKey(lenght, characters);
+console.log(randomChar);
