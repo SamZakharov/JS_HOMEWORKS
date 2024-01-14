7559919f47cfac623d7c94675d25947765065263
SamZakharov <semen.zaharovv@gmail.com>
2024-01-14 10:23:08 +0200

    Made the changes that were indicated. I replaced the constant on line 13 with a variable. And removed the return on line 9.

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,10 @@ const degree = +prompt('Enter the number of the degree:');
 function exponentiation(number, degree = 1) {
     console.log(degree);
     if (isNaN(number) || isNaN(degree)) return alert('You are entering an incorrect value');
-    else return Math.pow(number, degree);
+    return Math.pow(number, degree);
 
 }
 
-let result = exponentiation(number, degree);
+const result = exponentiation(number, degree);
 
 alert(result);
