af7b4d015133780101b537d7b5e1e17c42a507f6
SamZakharov <semen.zaharovv@gmail.com>
2024-01-15 16:50:14 +0200

    HW 20 is finished.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+
+const userNumber = prompt('Введіть дату вашого народження');
+const userAge = parseInt(userNumber);
+
+if (!isNaN(userAge)) {
+    let userAgeStr;
+
+    if (userAge >= 0) {
+        if (userAge > 10 && userAge < 20) {
+            userAgeStr = 'років';
+        } else {
+            switch (userAge % 10) {
+                case 1 :
+                    userAgeStr = 'рік';
+                    break;
+
+                case 2:
+                case 3:
+                case 4:
+                    userAgeStr = 'роки';
+                    break;
+
+                default:
+                    userAgeStr = 'років';
+                    break;
+            }
+        }
+
+        alert(`${userAge} ${userAgeStr}`);
+
+    } else {
+        alert('Введіть додатнє число.');
+    }
+
+} else {
+    alert('Введіть коректне число.');
+}
