2d25fe0e71de82ee6e0054f086a5a540034293c1
SamZakharov <semen.zaharovv@gmail.com>
2024-01-15 15:08:18 +0200

    I remade homework #28 following the example you showed in the lesson, but decided to leave the functionality that I had before. After debugging everything is worked.

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,26 @@
 'use strict';
 
-const simpleStr = prompt('Enter your word:');
-const lengthStr = prompt('Enter the number of length');
-const symbol = '*';
-const boolean = true;
+function padString(simpleStr, lengthStr, symbol, right = true) {
+    if (typeof simpleStr !== 'string') throw new Error('You haven\'t entered a word');
+    if (typeof lengthStr !== 'number' || isNaN(lengthStr)) throw new Error('You didn\'t entered the string length');
+    if (simpleStr.length === lengthStr) return simpleStr;
 
-function padString(simpleStr, lengthStr, symbol, boolean) {
-    if (!simpleStr) {
-        return console.log('You haven\'t entered a word');
-    } else if (!lengthStr) {
-        return console.log('You didn\'t entered the string length');
-    } else if (!symbol) {
-        return console.log('You didn\'t entered the symbol');
-    } else if (boolean === undefined) {
-        return console.log('You didn\'t  entered a parameter');
-    } else {
-        if (lengthStr > simpleStr.length) {
-            let n = lengthStr - simpleStr.length;
-            let strArr = simpleStr.split('');
-            for (let i = 0; i < n; i++) {
-                boolean ? strArr.push(symbol) : strArr.unshift(symbol);
-            }
-            return strArr.join('');
-        } else {
-            let j = simpleStr.length - lengthStr;
-            return boolean ? simpleStr.substring(j) : simpleStr.substring(0, simpleStr.length - j);
-        }
+    if (typeof symbol !== 'string' || symbol.length !== 1) throw new Error('You didn\'t entered the symbol');
+    if (typeof right !== 'boolean') throw new Error('Right should be a boolean');
+
+    if (simpleStr.length > lengthStr) {
+        const j = simpleStr.length - lengthStr;
+        return right ? simpleStr.substring(j) : simpleStr.substring(0, simpleStr.length - j);
     }
+    const n = lengthStr - simpleStr.length;
+    const strArr = simpleStr.split('');
+    for (let i = 0; i < n; i++) {
+        right ? strArr.push(symbol) : strArr.unshift(symbol);
+    }
+    return strArr.join('');
+
 }
 
-console.log(padString(simpleStr, lengthStr, symbol, true));
+console.log(padString('hello', 3, '*', false));
+console.log(padString('hello', 6, '*', ));
+
