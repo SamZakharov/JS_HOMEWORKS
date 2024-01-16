f4969a2fcdcc30a6ac704e487db3af31d735332e
SamZakharov <semen.zaharovv@gmail.com>
2024-01-16 16:01:22 +0200

    HW 30 is finished. Аdded code optimization.

--- src/js/script.js
+++ src/js/script.js
@@ -7,13 +7,7 @@ const potatoWeight = 0.075;
 const pricePerKg = 13;
 
 function calculatePotatoCost(litersOfBorsch, weightPerLiter, potatoesPerLiter, potatoWeight, pricePerKg) {
-    const totalPotatoes = litersOfBorsch * potatoesPerLiter;
-
-    const totalWeight = totalPotatoes * potatoWeight;
-
-    const totalPotatoInKg = Math.ceil(weightPerLiter / potatoWeight);
-
-    return totalPotatoInKg * pricePerKg;
+    return Math.ceil(weightPerLiter / potatoWeight) * pricePerKg;
 }
 
 const result = calculatePotatoCost(litersOfBorsch, weightPerLiter, potatoesPerLiter, potatoWeight, pricePerKg);
