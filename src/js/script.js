26537e0498ee1c39abc7b6ba31b5faeb9519f6ce
SamZakharov <semen.zaharovv@gmail.com>
2024-01-16 15:37:40 +0200

    HW 30 is finished.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const litersOfBorsch = 48;
+const weightPerLiter = 1;
+const potatoesPerLiter = 4;
+const potatoWeight = 0.075;
+const pricePerKg = 13;
+
+function calculatePotatoCost(litersOfBorsch, weightPerLiter, potatoesPerLiter, potatoWeight, pricePerKg) {
+    const totalPotatoes = litersOfBorsch * potatoesPerLiter;
+
+    const totalWeight = totalPotatoes * potatoWeight;
+
+    const totalPotatoInKg = Math.ceil(weightPerLiter / potatoWeight);
+
+    return totalPotatoInKg * pricePerKg;
+}
+
+const result = calculatePotatoCost(litersOfBorsch, weightPerLiter, potatoesPerLiter, potatoWeight, pricePerKg);
+console.log(`Вартість покупки картоплі: ${result} грн`);
