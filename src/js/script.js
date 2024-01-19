92aa1fb7a2b2cbf3ec5224198aafd83c46cf8b2b
SamZakharov <semen.zaharovv@gmail.com>
2024-01-19 16:50:46 +0200

    HW 31 is finished.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function createRandomNumber() {
+    const usedNumbers = [];
+
+    return function getRandomNumber() {
+        const maxNumber = 100;
+        let randomNumber;
+
+        do {
+            randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+        } while (usedNumbers.includes(randomNumber));
+
+        usedNumbers.push(randomNumber);
+
+        if (usedNumbers.length === maxNumber) usedNumbers.length = 0;
+
+        return randomNumber;
+    };
+}
+
+const getRandomNumber = createRandomNumber();
+
+for (let i = 0; i < 10; i++) {
+    console.log(getRandomNumber());
+}
