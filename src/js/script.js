e43ec5ea8957a6e989bfc4a347c1488448c2a515
SamZakharov <semen.zaharovv@gmail.com>
2024-01-29 15:16:07 +0200

    HW 34 is finished.

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,26 @@
-const createUser = function createUser(name, age) {
-  const user = {};
-
-  user.name = name;
-  user.age = age;
-
-  user.greet = function () {
-    console.log(`Hello, I'm ${this.name}, and I'm ${this.age} y.o.`);
+const init = function () {
+  const sayHi = function () {
+    console.log('hello');
   };
 
-  return user;
+  return function createUser(name, age) {
+    const lastName = 'Doe';
+
+    return {
+      name,
+      age,
+      lastName,
+      sayHi,
+    };
+  };
 };
 
-const user1 = createUser('Sam', 28);
-const user2 = createUser('Vika', 32);
-const user3 = createUser('Vova', 52);
+const createUser = init();
 
-user1.greet();
-user2.greet();
-user3.greet();
+const user = createUser('John', 25);
+const user2 = createUser('Alice', 24);
+const user3 = createUser('Alice', 32);
+
+user.sayHi();
+user2.sayHi();
+user3.sayHi();
