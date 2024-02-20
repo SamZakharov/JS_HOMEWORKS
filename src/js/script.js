37f16d0a8235df8060305b1f12830e4dc29c3b59
SamZakharov <semen.zaharovv@gmail.com>
2024-02-20 21:30:44 +0200

    HW 48 is finished. This is a very good practice, it was actually interesting to do it.

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+// First task
+const arrowFunc1 = (a, b) => a + b;
+console.log(arrowFunc1(1, 2));
+
+// Second task
+const arrowFunc2 = (str) => str.length;
+console.log(arrowFunc2('Some string'));
+
+// Third task
+const arrowFunc3 = (arr) => arr.map((el) => el + 1);
+console.log(arrowFunc3([1, 2, 3, 4]));
